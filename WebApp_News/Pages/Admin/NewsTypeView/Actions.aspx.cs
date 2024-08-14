96e0020af6e6cba4be49a518dde3ffa01e6015ce
HHY88H <hamedhajimohammadi11@gmail.com>
2024-08-14 21:36:31 +0330

    Add ,Edit and Delete for NewsType has been completed

--- WebApp_News/Pages/Admin/NewsTypeView/Actions.aspx.cs
+++ WebApp_News/Pages/Admin/NewsTypeView/Actions.aspx.cs
@@ -11,7 +11,66 @@ namespace WebApp_News.Pages.Admin.NewsTypeView
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.QueryString["ID"] == null)
+            {
+                Response.Redirect("~/pages/Error/E404.html", true);
+                return;
+            }
+            string StrID = Request.QueryString["ID"];
+            if (StrID == "-1")
+            {
+                btnEdit.Visible = false;
+                btnDelete.Visible = false;
+            }
+            else
+            {
+                if (!IsPostBack)
+                {
+                    GetData();
+                    btnNew.Visible = false;
+                }
+            }
+        }
+        private void GetData()
+        {
+            string StrID = Request.QueryString["ID"];
+            int ID = int.Parse(StrID);
+            Models.DBNewsEntities1 dbcontext = new Models.DBNewsEntities1();
+            var Query = dbcontext.NwesTypes.Where(nt => nt.ID_NewsType == ID).FirstOrDefault();
+            TxtTitle.Value = Query.Title_NewsType;
 
+        }
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            string StrID = Request.QueryString["ID"];
+            int ID = int.Parse(StrID);
+            Models.DBNewsEntities1 dbcontext = new Models.DBNewsEntities1();
+            var Query = dbcontext.NwesTypes.Where(nt => nt.ID_NewsType == ID).FirstOrDefault();
+            Query.Title_NewsType = TxtTitle.Value;
+            dbcontext.SaveChanges();
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "PageNew", "  window.close();window.opener.location.reload();", true);
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            string StrID = Request.QueryString["ID"];
+            int ID = int.Parse(StrID);
+            Models.DBNewsEntities1 dbcontext = new Models.DBNewsEntities1();
+            var Query = dbcontext.NwesTypes.Where(nt => nt.ID_NewsType == ID).FirstOrDefault();
+            dbcontext.NwesTypes.Remove(Query);
+            dbcontext.SaveChanges();
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "PageNew", "  window.close();window.opener.location.reload();", true);
+        }
+
+        protected void btnNew_Click1(object sender, EventArgs e)
+        {
+            Models.DBNewsEntities1 dbcontext = new Models.DBNewsEntities1();
+            Models.NwesType nt = new Models.NwesType();
+            nt.Title_NewsType = TxtTitle.Value;
+            dbcontext.NwesTypes.Add(nt);
+            dbcontext.SaveChanges();
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "PageNew", " window.opener.location.reload();", true);
+            TxtTitle.Value = "";
         }
     }
 }
